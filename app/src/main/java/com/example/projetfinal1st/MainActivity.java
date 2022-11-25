d559adaef031223ba6d2e80dc080e4b6e0834330
Biscu8 <jerleyo7$@gmail.com>
2022-11-24 19:51:21 -0500

    Login card and buttons

--- app/src/main/java/com/example/projetfinal1st/MainActivity.java
+++ app/src/main/java/com/example/projetfinal1st/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.projetfinal1st;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+        * Button to change background from dark mode to white mode / white mode to dark mode
+        */
+        Button button = findViewById(R.id.colorButton)
+        button.setOnClickListener(new View.OnClickListener() {}
+
     }
+
+
 }
