fd0721c34aabca018ff996a90f7c52c63714e4f3
Biscu8 <jerlagueux@gmail.com>
2022-11-25 00:26:37 -0500

    Theme change button

--- app/src/main/java/com/example/projetfinal1st/MainActivity.java
+++ app/src/main/java/com/example/projetfinal1st/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.projetfinal1st;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +20,23 @@ public class MainActivity extends AppCompatActivity {
 
         /**
         * Button to change background from dark mode to white mode / white mode to dark mode
-        */
-        Button button = findViewById(R.id.colorButton)
-        button.setOnClickListener(new View.OnClickListener() {}
+        *///TODO
+        Button button = findViewById(R.id.colorButton);
+        int i = 0;
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            public void onClick(View view) { // this doesnt work **
+                RelativeLayout layout = findViewById(R.id.relativeLayout);
+                if (i ==    0) {
+                    layout.setBackgroundColor(R.color.main_color_black);
+                    i = 1;
+                } else {
+                    layout.setBackgroundColor(R.color.main_color_white);
+                    i = 0;
+                }
+            }
+        });
 
     }
-
 
 }
