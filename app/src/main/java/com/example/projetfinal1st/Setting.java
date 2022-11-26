35d21df4d88985f44dbe5746ecfc9f611cc08086
boudr <boudreaultca@gmail.com>
2022-11-26 13:11:10 -0500

    tentative de faire la traduction si vous voulez accedez au parametre sans tout faire crash mettez en commentaire la ligne 24-25 et les lignes 58-71 dans le setting.java

--- app/src/main/java/com/example/projetfinal1st/Setting.java
+++ app/src/main/java/com/example/projetfinal1st/Setting.java
@@ -2,15 +2,27 @@ package com.example.projetfinal1st;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.view.textclassifier.TextClassifierEvent;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+
+import java.util.Locale;
 
 public class Setting extends AppCompatActivity {
     private RadioGroup group;
     private ImageView imageView;
-
+    //pour le changement de langue
+    /**
+     * Resources va chercher le fichier ressource de lapplication
+     * Config prend la configuration des ressources
+     */
+    private Resources res = getApplicationContext().getResources();
+    private Configuration config = res.getConfiguration();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_setting);
@@ -20,6 +32,9 @@ public class Setting extends AppCompatActivity {
         group = findViewById(R.id.bouttongroup);
         group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
+            /**
+             * la fonction verifie quelle boutton est selectionner pour ensuite changer limage selon la difficulte
+             */
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 switch(i)
                 {
@@ -35,9 +50,25 @@ public class Setting extends AppCompatActivity {
                 }
             }
         });
-
-
-
+        /**
+         * fonction qui verifie si le boutton anglais est utiliser
+         * sil est utiliser on change la configuration pour langlais
+         * cela va faire reload lapp et nous devrons la relancer avec langlais
+         */
+        findViewById(R.id.anglais).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                config.locale = new Locale("en-rCA");
+                res.updateConfiguration(config, res.getDisplayMetrics());
+            }
+        });
+        findViewById(R.id.francais).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                config.locale = new Locale("fr-rCA");
+                res.updateConfiguration(config, res.getDisplayMetrics());
+            }
+        });
     }
 
 
